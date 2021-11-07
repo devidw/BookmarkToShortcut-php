c03351763d6f5d6c7e0c24bb34605c0cb28d40f6
devidwolf <68775926+devidw@users.noreply.github.com>
2021-11-07 16:26:41 +0100

    make php only

--- python/test.py
+++
@@ -1,8 +0,0 @@
-from BookmarkToShortcut import BookmarkToShortcut
-
-converter = BookmarkToShortcut(
-    'tests/in', # input directory
-    'tests/out', # output directory
-    {'url', 'desktop', 'webloc'} # formats to write
-)
-converter.convert()
